5fec454ec7de2485c3912a866d65255e426602d4
exec life <exec_life@protonmail.com>
2021-03-04 19:00:54 -0500

    Updated tcp client

--- tcp_client.py
+++ tcp_client.py
@@ -2,12 +2,13 @@ import socket
 
 target_host = input("Please enter a host to connect to: ")
 short_host = target_host[4:]
-target_port = 80
+target_port = int(input("Please enter a port to connect to: "))
 d = f"GET / HTTP/1.1\r\nHost: {short_host}\r\n\r\n"
 data = d.encode("utf-8")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
+print("Connection to host successful")
 client.send(data)
 resp = client.recv(4096)
 response = resp.decode("utf-8")
